f4522477594eef0e2c11347113a35e201a2b5e8e
Ilan Aragão <ilan_aragao@hotmail.com>
2021-06-23 18:13:40 -0300

    Exercicio 3 OK

---
+++ bloco_4/dia_1/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 3;
+let c = 1;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+};
