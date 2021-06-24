fe674f521e53048814c5c8e53592977d1df59d3a
Ilan Aragão <ilan_aragao@hotmail.com>
2021-06-24 16:40:29 -0300

    Exercicio 9 OK

---
+++ bloco_4/dia_2/exercicio9.js
@@ -0,0 +1,9 @@
+let numeros = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numeros.push(index);
+}
+
+for (let indice = 0; indice < numeros.length; indice += 1) {
+    console.log(numeros[indice] / 2);
+  };
