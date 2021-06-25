38ac0e080438c01ba0e286673ceb231c7b962256
Ilan Aragão <ilan_aragao@hotmail.com>
2021-06-25 18:46:12 -0300

    Exercicio 1 OK

---
+++ bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let asteriscos = '*';
+let linha = ' ';
+
+for (let index = 0; index < n; index += 1) {
+    linha = linha + asteriscos    
+};
+
+for (let index = 0; index < n; index += 1) {
+console.log(linha);
+};
