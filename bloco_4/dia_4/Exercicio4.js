f2d7a00074d30e88556774ea3218b4430f1d2464
Ilan Aragão <ilan_aragao@hotmail.com>
2021-06-28 22:19:36 -0300

    Exercicio 5 OK

--- bloco_4/dia_4/Exercicio4.js
+++ bloco_4/dia_4/Exercicio4.js
@@ -1,11 +1,11 @@
 let info = {
-    personagem: "Margarida",
-    origem: "Pato Donald",
-    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
-  
-  info.recorrente = 'Sim'
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
 
-  for (const key in info) {
-      console.log(info[key]);
-  }
+info.recorrente = "Sim";
+
+for (const key in info) {
+  console.log(info[key]);
+}
