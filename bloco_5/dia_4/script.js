97c9e97ed1b59e9d948eb2031b9517ba8c70a4fa
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-07 19:10:15 -0300

    Parte 2 OK

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,7 +1,15 @@
 /* Questão 1 */
 
-const select = document.querySelector('select');
-select.addEventListener('change', function() {
-    const cor = select.selectedOptions[0] 
+const corFundo = document.querySelector('#select-back');
+corFundo.addEventListener('change', function() {
+    const cor = corFundo.selectedOptions[0] 
     document.body.style.backgroundColor = cor.value
+})
+
+/* Questão 2 */
+
+const corLetra = document.querySelector('#select-color');
+corLetra.addEventListener('change', function() {
+    const mudaCor = corLetra.selectedOptions[0] 
+    document.body.style.color = mudaCor.value
 })
