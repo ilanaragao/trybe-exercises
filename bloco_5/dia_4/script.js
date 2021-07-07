b5fcbbbae1083ee66f6597838a5be3ab021b3535
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-07 19:35:52 -0300

    Parte 3 e melhorias gerais OK

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,15 +1,39 @@
 /* Questão 1 */
 
-const corFundo = document.querySelector('#select-back');
-corFundo.addEventListener('change', function() {
-    const cor = corFundo.selectedOptions[0] 
-    document.body.style.backgroundColor = cor.value
-})
+const corFundo = document.querySelector("#select-back");
+corFundo.addEventListener("change", function () {
+  const cor = corFundo.selectedOptions[0];
+  document.body.style.backgroundColor = cor.value;
+
+  localStorage.setItem('background-color', cor.value)
+});
+
+const backgroundSalvo = localStorage.getItem('background-color');
+document.body.style.backgroundColor = backgroundSalvo
 
 /* Questão 2 */
 
-const corLetra = document.querySelector('#select-color');
-corLetra.addEventListener('change', function() {
-    const mudaCor = corLetra.selectedOptions[0] 
-    document.body.style.color = mudaCor.value
+const corLetra = document.querySelector("#select-color");
+corLetra.addEventListener("change", function () {
+  const mudaCor = corLetra.selectedOptions[0];
+  document.body.style.color = mudaCor.value;
+
+  localStorage.setItem('font-color', mudaCor.value)
+});
+
+const corSalva = localStorage.getItem('font-color');
+document.body.style.color = corSalva;
+
+/* Questão 3 */
+
+const tamanhoFonte = document.querySelector('input[name="tamanho-fonte"]');
+tamanhoFonte.addEventListener('change', function () {
+    const p = document.querySelector('p');
+    p.style.fontSize = `${tamanhoFonte.value}px`;
+
+    localStorage.setItem('font-size', `${tamanhoFonte.value}px`);
 })
+
+const tamanhoSalvo = localStorage.getItem('font-size');
+const p = document.querySelector('p');
+p.style.fontSize = tamanhoSalvo;
