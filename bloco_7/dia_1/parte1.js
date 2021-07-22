5abd786025194614eebfddca25343fbe18f00460
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-22 18:50:50 -0300

    Adiciona exercício 2 da primeira parte

--- bloco_7/dia_1/parte1.js
+++ bloco_7/dia_1/parte1.js
@@ -12,3 +12,23 @@ const testingScope = (escopo) => {
 };
 
 testingScope(true);
+
+/* Exercício 2 */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+};
+
+const sortedArray = sortOddsAndEvens();
+console.log(
+  `Os números ${sortedArray} se encontram ordenados de forma crescente !`
+);
