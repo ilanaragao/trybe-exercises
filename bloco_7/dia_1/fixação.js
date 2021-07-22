ad9d5ea3b53623be9f766786e3c5750f262c1c68
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-22 16:11:28 -0300

    Adiciona fixação parte 1

---
+++ bloco_7/dia_1/fixação.js
@@ -0,0 +1,11 @@
+function userInfo() {
+  let userEmail = "maria@email.com";
+  console.log(userEmail);
+}
+userInfo();
+
+if (true) {
+  var userAge = "20";
+  console.log(userAge);
+}
+console.log(userAge);
