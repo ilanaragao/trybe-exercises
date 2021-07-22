f9552a6686fd5ac692e7d0222e0bfb1341e1ad5f
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-22 18:58:35 -0300

    Adiciona exercício 4 da segunda parte

--- bloco_7/dia_1/parte2.js
+++ bloco_7/dia_1/parte2.js
@@ -32,3 +32,35 @@ const longestWord = (text) => {
 console.log(
   longestWord("Antonio foi no banheiro e não sabemos o que aconteceu")
 );
+
+/* Exercício 4 */
+
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+function buildSkillsPhrase(paramOne) {
+  const fun1 = (paramInner) =>
+    `Tryber ${paramInner} aqui!
+
+      Tudo bem?`;
+
+  let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+  array.forEach(
+    (skill, index) =>
+      (result = `${result}
+
+    - ${skill}`)
+  );
+
+  result = `
+${result}
+
+    #goTrybe
+    `;
+
+  return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
