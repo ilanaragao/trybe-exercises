cd5620cca35b203081f10ce335c05553eced5ce0
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-24 17:20:26 -0300

    Adiciona exercício 5 parte 2

--- bloco_7/dia_2/script2.js
+++ bloco_7/dia_2/script2.js
@@ -32,3 +32,6 @@ console.log(sizeObj(lesson1));
 
 const listValues = (obj) => Object.values(obj);
 console.log(listValues(lesson1));
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
