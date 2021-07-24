e16f43ed5551099d2e6d26ac3a120a950ff0bd50
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-24 17:38:29 -0300

    Adiciona exercício 6 parte 2

--- bloco_7/dia_2/script2.js
+++ bloco_7/dia_2/script2.js
@@ -35,3 +35,13 @@ console.log(listValues(lesson1));
 
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
+
+const getNumberOfStudents = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+console.log(getNumberOfStudents(allLessons));
