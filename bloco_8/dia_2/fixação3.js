fb3e4ac8002a01fd613d7e201d73f6b5f9430740
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-29 15:49:15 -0300

    Adiciona fixação 3.2

--- bloco_8/dia_2/fixação3.js
+++ bloco_8/dia_2/fixação3.js
@@ -1,3 +1,5 @@
+/* Fixação 1 */
+
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (array, nome) => {
@@ -6,3 +8,20 @@ const hasName = (array, nome) => {
 };
 
 console.log(hasName(names, 'Ana'));
+
+/* Fixação 2 */
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((person) => person.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
