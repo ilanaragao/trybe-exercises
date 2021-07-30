dcd9771d57ba3dca1f1c7be9be9e1f8f77f6f021
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-30 14:53:41 -0300

    Adiciona exercícios de fixação do dia 8.3

---
+++ bloco_8/dia_3/fixação.js
@@ -0,0 +1,25 @@
+/* Mostrar apenas as pessoas que não possuem ainda idade para dirigir, utilizando o 'filter' */
+
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) =>
+  arrayOfPeople.filter((people) => people.age < 18);
+
+console.log(verifyAgeDrive(objPeople));
+
+/* Remover elemento do objeto utilizando o 'filter' */
+
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+// Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents);
