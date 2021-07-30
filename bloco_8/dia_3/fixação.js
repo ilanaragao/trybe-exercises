4cadf275d8433d4c206ca60a437aecf608741f7b
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-30 15:14:11 -0300

    Adiciona exercícios de fixação do dia 8.3

--- bloco_8/dia_3/fixação.js
+++ bloco_8/dia_3/fixação.js
@@ -11,7 +11,7 @@ const objPeople = [
 const verifyAgeDrive = (arrayOfPeople) =>
   arrayOfPeople.filter((people) => people.age < 18);
 
-console.log(verifyAgeDrive(objPeople));
+console.log(verifyAgeDrive(objPeople)); // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
 
 /* Remover elemento do objeto utilizando o 'filter' */
 
@@ -22,4 +22,142 @@ const removeStudentByName = (name, listStudents) =>
 // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
 
 const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
-console.log(newListStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+/* Percorrendo array usando o 'map' */
+
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map(
+  (person) => `${person.firstName} ${person.lastName}`
+);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+/* Retornar numéros negativos do array, quando maior que zero, utilizando o  'map' */
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) =>
+  number > 0 ? number * -1 : number
+);
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+/* Retornar numéros negativos do array, quando maior que zero, utilizando o  'map' */
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) =>
+  listProducts.map((product, index) => ({ [product]: listPrices[index] }));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+/* Filtrar os estudantes que estudam no período da manhã usando 'filter' e 'map' */
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' },
+    ],
+  },
+];
+
+const allNameStudents = estudantes
+  .filter((estudante) => estudante.turno === 'Manhã')
+  .map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+
+console.log(allNameStudents);
+
+/* Buscar um estudante pelo nome e retornar a situação dele em cada matéria com 'find' e 'map' */
+
+const reportStatus = (name, students) => {
+  const studentInfo = students.find((student) => student.nome === name);
+  return studentInfo.materias.map(
+    (materia) =>
+      `${materia.name} ${materia.nota >= 60 ? 'Aprovado' : 'Reprovado'}`
+  );
+};
+
+console.log(reportStatus('Natalia', estudantes));
