445be29470d66ccef619c491ec6d43a73dc2055c
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-31 19:09:21 -0300

    Adiciona exercício de fixação do dia 8.4

--- bloco_8/dia_4/fixação.js
+++ bloco_8/dia_4/fixação.js
@@ -10,3 +10,124 @@ console.log(sumNumbers); // 113
 const getSum = (result, number) => result + number;
 const sumNumbers = numbers.reduce(getSum);
 console.log(sumNumbers); // 113
+
+/* Comparar valores para buscar o maior valor em um array. */
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => (bigger > number ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger); // 85
+
+/* função que some todos os números pares do array (USANDO FILTER E REDUCE) */
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (currentValue, number) => currentValue + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+console.log(sumNumbers(numbers)); // 152
+
+/* função que some todos os números pares do array (SEGUNDA FORMA DE FAZER SEM O FILTER) */
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPair = (currentValue, number) =>
+  number % 2 === 0 ? currentValue + number : currentValue;
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+console.log(sumNumbers(numbers)); // 152
+
+/* Mostrar na tela um relatório que diz em qual matéria o estudante foi melhor */
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const getBestClass = (acc, materia) => {
+  if (acc.nota > materia.nota) return acc;
+  return materia;
+};
+
+const reportBetter = (students) =>
+  students.map((student) => ({
+    name: student.nome,
+    materia: student.materias.reduce(getBestClass).name,
+  }));
+
+console.log(reportBetter(estudantes));
