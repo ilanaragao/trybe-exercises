35a22aa01425a889e5aec2d003784a455f5bccbd
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-01 17:49:33 -0300

    Adiciona exercício 1 do dia 8.4

---
+++ bloco_8/dia_4/exercício1.js
@@ -0,0 +1,15 @@
+/* Dada uma matriz, transforme em um array. */
+
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acumulador, value) => acumulador.concat(value), [])
+};
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
