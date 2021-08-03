0b1cfeede11d4f4dc8cdc12f0346558e4a9694d4
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-03 15:46:38 -0300

    Fixação 3 da parte 2

--- bloco_8/dia_5/fixação2.js
+++ bloco_8/dia_5/fixação2.js
@@ -15,7 +15,16 @@ let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
 
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
 [comida, animal, bebida] = [bebida, comida, animal];
 console.log(comida, animal, bebida); // arroz gato água
 
-// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+/* O array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares . */
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[ , , , ...numerosPares] = numerosPares;
+console.log(numerosPares); // [6, 8, 10, 12];
