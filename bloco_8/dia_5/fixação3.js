41bc6a0d2ab889c2475f5d49b361eb4656c9aacf
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-03 16:03:17 -0300

    Fixação da parte 4

--- bloco_8/dia_5/fixação3.js
+++ bloco_8/dia_5/fixação3.js
@@ -1,6 +1,7 @@
 /* Do jeito que está, quando passamos person para a função getNationality o retorno é João is undefined . Ajuste a função getNationality para que a chamada getNationality(person) retorne João is Brazilian . */
 
-const getNationality = ({ firstName, lastName, nationality = "Brazilian" }) => `${firstName} ${lastName} is ${nationality}`;
+const getNationality = ({ firstName, lastName, nationality = 'Brazilian' }) =>
+  `${firstName} ${lastName} is ${nationality}`;
 
 const person = {
   firstName: 'João',
@@ -13,7 +14,7 @@ const otherPerson = {
   nationality: 'Russian',
 };
 
-const { nationality = "Brazilian" } = person;
+const { nationality = 'Brazilian' } = person;
 
 console.log(getNationality(otherPerson)); // Ivan is Russian
 console.log(getNationality(person)); // João is Brazilian
