e928a814916637c648827d360d6e63320103ed1a
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-03 15:44:04 -0300

    Fixação 2 da parte 2

--- bloco_8/dia_5/fixação2.js
+++ bloco_8/dia_5/fixação2.js
@@ -8,3 +8,14 @@ saudacoes[1](saudacoes[0]); // Olá
 const [saudacao, realizaSaudacao] = saudacoes;
 
 console.log(realizaSaudacao(saudacao)); // Olá
+
+/* A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis. */
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
