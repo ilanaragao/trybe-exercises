23786c3a61f8e884dd99acc0cd72dd40b07548f9
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-11 12:20:39 -0300

    Adiciona exercício 2 do dia 9.1

--- fundamentos/bloco-09-javaccript-assíncrono-e-promises/dia-1/exercício2.js
+++ fundamentos/bloco-09-javaccript-assíncrono-e-promises/dia-1/exercício2.js
@@ -1,4 +1,4 @@
-/* Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas? */
+/* Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas? */
 
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`;
@@ -28,5 +28,5 @@ const jupiter = {
 };
 
 console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
