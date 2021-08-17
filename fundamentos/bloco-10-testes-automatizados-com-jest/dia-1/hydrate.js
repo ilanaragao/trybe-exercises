a684a3cfc5ec6b3e3cd6934f2720f69688dd74ac
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-17 19:09:46 -0300

    Adiciona exercício 3 da parte 2

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1/hydrate.js
@@ -0,0 +1,33 @@
+/* A função hydrate recebe uma string no formato "numero bebida", e retorna a sugestão de quantos copos de água você deve beber para se hidratar. Para cada bebida, deve-se tomar um copo de água para não ter ressaca. Exemplo:
+// string recebida
+'1 cerveja'
+// retorno da função
+'1 copo de água'
+
+// string recebida
+'1 cerveja, 2 shots e 1 catuaba'
+// retorno da função
+'4 copos de água'
+
+// string recebida
+'2 caipirinhas'
+// retorno da função
+'2 copos de água'
+*/
+
+const hydrate = (string) => {
+  const splitString = string.split('');
+  const result = splitString.reduce((glassesOfWater, character) => {
+    let parsedCharacter = parseInt(character);
+    let checkCharacter = isNaN(parsedCharacter) === false;
+    if (checkCharacter) {
+      return glassesOfWater + parsedCharacter;
+    }
+    return glassesOfWater;
+  }, 0);
+
+  const pluralGlass = result === 1 ? 'copo' : 'copos';
+  return `${result} ${pluralGlass} de água`;
+};
+
+module.exports = hydrate;
