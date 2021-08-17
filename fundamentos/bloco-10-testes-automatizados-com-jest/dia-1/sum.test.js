8a73f8c228f0aece8485b8746298c41a5bdc9a7d
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-17 18:43:37 -0300

    Adiciona exercício 2

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1/sum.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1/sum.test.js
@@ -3,7 +3,11 @@
 const sum = require('./sum');
 
 describe(`Teste para a função 'sum':`, () => {
-  test('4 mais 5 é igual a 9', () => {
+  it('4 mais 5 é igual a 9', () => {
     expect(sum(4, 5)).toBe(9);
+  });
+  /* 2 - Teste se o retorno de sum(0, 0) é 0 */
+  it('0 mais 0 é igual 0', () => {
+    expect(sum(0, 0)).toBe(0);
   });
 });
