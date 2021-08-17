b3dd7e54d43ce8dc3c9d6d4cf7d83e8c9ac674f9
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-17 18:49:29 -0300

    Adiciona exercício 4

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1/sum.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1/sum.test.js
@@ -10,4 +10,16 @@ describe(`Teste para a função 'sum':`, () => {
   it('0 mais 0 é igual 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
+  /* 3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5) */
+  it('Quando os parâmetros são 4 e "5", a função retorna um erro', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+  /* 4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5") */
+  it('Testa se a mensagem de erro é: "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError('parameters must be numbers');
+  });
 });
