f33f598f243016df6ecd963ce76eb179ed15c5ae
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-19 15:52:42 -0300

    Adiciona exercício de fixação 4

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3/math.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3/math.test.js
@@ -21,8 +21,8 @@ test("multiplicar com retorno padrão de valor '10'", () => {
 
 /* Faça o mock da função somar e implemente uma função que recebe dois valores e retorna sua soma. Teste a chamada, o retorno e os parâmetros passados. */
 
-test("Testa chamada, retorno e parâmetros passados para soma com entrada de dois valores", async () => {
-  const mockSomar = jest.spyOn(math, "somar");
+test('Testa chamada, retorno e parâmetros passados para soma com entrada de dois valores', async () => {
+  const mockSomar = jest.spyOn(math, 'somar');
   mockSomar.mockResolvedValue(5);
 
   mockSomar(2, 3);
@@ -30,4 +30,28 @@ test("Testa chamada, retorno e parâmetros passados para soma com entrada de doi
   expect(mockSomar).toHaveBeenCalledTimes(1);
   expect(mockSomar).toHaveBeenCalledWith(2, 3);
   await expect(mockSomar(2, 3)).resolves.toBe(5);
+});
+
+/* Faça o mock da função dividir e implemente um retorno padrão com o valor ‘15’. Implemente também os seguintes valores para a primeira e segunda chamadas: ‘2’ e ‘5’. Teste a chamada, o retorno, os parâmetros e quantas vezes a função foi chamada. */
+
+test('Testa chamada, retorno e parâmetros da função dividir', () => {
+  const mockDividir = jest.spyOn(math, 'dividir');
+  mockDividir.mockReturnValue(15);
+  mockDividir.mockReturnValueOnce(2);
+  mockDividir.mockReturnValueOnce(5);
+
+  expect(mockDividir(4, 2)).toBe(2);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(1);
+  expect(mockDividir).toHaveBeenCalledWith(4, 2);
+
+  expect(mockDividir(10, 2)).toBe(5);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(2);
+  expect(mockDividir).toHaveBeenCalledWith(10, 2);
+
+  expect(mockDividir(30, 2)).toBe(15);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(3);
+  expect(mockDividir).toHaveBeenCalledWith(30, 2);
 });
