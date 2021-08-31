a09d61a03f9574998556c04c93dd9c2adb04ba9a
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-31 15:29:15 -0300

    Exercício fixação composição

--- front-end/bloco-11-introdução-à-react/dia-2/test/src/index.js
+++ front-end/bloco-11-introdução-à-react/dia-2/test/src/index.js
@@ -2,14 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Greeting from './components/Greeting';
-import Image from './components/Image'
+import Image from './components/Image';
+import Album from './components/Album';
 import reportWebVitals from './reportWebVitals';
+
+const album01 = {
+  image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+  title: 'Mylo Xyloto',
+  releaseDate: {
+    year: '2011',
+    month: '10',
+    day: '24',
+  },
+  others: {
+    recordCompany: 'Capitol, Parlophone',
+    formats: 'CD, digital'
+  }
+};
+
+const album02 = {
+  image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+  title: 'Ghost Stories',
+  releaseDate: {
+    year: '2014',
+    month: '05',
+    day: '16',
+  },
+  others: {
+    recordCompany: 'Parlophone',
+    formats: 'CD, digital'
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <>
-    <Greeting name='Ilan' lastName='Aragão'/>
-    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+      <Greeting name="Ilan" lastName="Aragão" />
+      <Image
+        source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+        alternativeText="Cute cat staring"
+      />
+      <Album album={album01} />
+      <Album album={album02} />
     </>
   </React.StrictMode>,
   document.getElementById('root')
