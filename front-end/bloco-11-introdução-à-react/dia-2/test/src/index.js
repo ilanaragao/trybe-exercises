1eb8080c43c0529a8a47da0f2114a011e6feedfb
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-31 15:16:36 -0300

    Exercício de fixação props

--- front-end/bloco-11-introdução-à-react/dia-2/test/src/index.js
+++ front-end/bloco-11-introdução-à-react/dia-2/test/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Greeting from './components/Greeting';
+import Image from './components/Image'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <>
     <Greeting name='Ilan' lastName='Aragão'/>
+    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
