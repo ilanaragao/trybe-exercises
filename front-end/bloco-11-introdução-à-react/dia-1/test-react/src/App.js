09c7e8329060c063527000a46c92e154665c4356
Ilan Aragão <ilan_aragao@hotmail.com>
2021-08-30 21:15:10 -0300

    Adiciona exercício 2 do dia 11.1

--- front-end/bloco-11-introdução-à-react/dia-1/test-react/src/App.js
+++ front-end/bloco-11-introdução-à-react/dia-1/test-react/src/App.js
@@ -1,3 +1,8 @@
+/* Crie uma lista de tarefas simples que:
+Utilize a função Task dentro do componente App .
+Leia as tarefas de dentro de um Array.
+Use a função map para criar cada item da lista no HTML. */
+
 import React from 'react';
 import './App.css';
 
@@ -9,7 +14,7 @@ const tasks = ['Acordar', 'Trabalhar', 'Estudar', 'Dormir', 'Repeat'];
 
 class App extends React.Component {
   render() {
-    return <ul>{ tasks.map((tarefas) => Task(tarefas)) }</ul>;
+    return <ul>{ tasks.map(tarefa => Task(tarefa)) }</ul>;
   }
 }
 
