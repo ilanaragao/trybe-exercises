8cb5ca654a129326e08b0d8d8c715688157c5998
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-02 20:48:12 -0300

    Fixação 3, 4, 5 e 6

--- front-end/bloco-12-componentes-com-estado-eventos-e-formulários-com-react/dia-1/test-react/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formulários-com-react/dia-1/test-react/src/App.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 
-function handleClickOne() {
-  console.log('REACT É O MELHOR!');
-}
-
-function handleClickTwo() {
-  console.log('REACT É RUIM!');
-}
-
-function handleClickThree() {
-  console.log('REACT É O MAAAARAVILHOSO!');
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+
+    this.state = {
+      countOne: 0,
+      countTwo: 0,
+      countThree: 0,
+    };
+  }
+
+  handleClickOne() {
+    this.setState(() => ({
+      countOne: 1,
+    }));
+  }
+
+  handleClickTwo() {
+    this.setState(() => ({
+      countTwo: 1,
+    }));
+  }
+
+  handleClickThree() {
+    this.setState(() => ({
+      countThree: 1,
+    }));
+  }
+
   render() {
     return (
       <div>
-        <button onClick={handleClickOne}>Me click!</button>
-        <button onClick={handleClickTwo}>Me click!</button>
-        <button onClick={handleClickThree}>Me click!</button>
+        <button onClick={this.handleClickOne}>Me click!</button>
+        <button onClick={this.handleClickTwo}>Me click!</button>
+        <button onClick={this.handleClickThree}>Me click!</button>
       </div>
     );
   }
