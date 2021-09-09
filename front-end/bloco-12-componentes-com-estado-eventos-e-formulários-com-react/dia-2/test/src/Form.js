358199f75ce9736e21277db7f7e31106b0077909
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-09 16:34:26 -0300

    Exercício 6 do dia 12.2

--- front-end/bloco-12-componentes-com-estado-eventos-e-formulários-com-react/dia-2/test/src/Form.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formulários-com-react/dia-2/test/src/Form.js
@@ -23,59 +23,75 @@ class Form extends Component {
     });
   }
 
+  renderSelectInput() {
+    const { age } = this.state;
+
+    return (
+      <label htmlFor="age">
+        Idade:
+        <select
+          id="age"
+          name="age"
+          onChange={ this.handleChange }
+          value={ age }
+        >
+          <option value="">Selecione</option>
+          <option value="adult">Maior que 18</option>
+          <option value="underage">Menor que 18</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { name, email, age, anecdote, terms } = this.state;
+    const { name, email, anecdote, terms } = this.state;
 
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="form">
+          <fieldset>
+            <legend>Informações pessoais</legend>
 
-          <label htmlFor="name">
-            Nome:
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={ this.handleChange }
-              value={ name }
-            />
-          </label>
+            <label htmlFor="name">
+              Nome:
+              <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={ this.handleChange }
+                value={ name }
+              />
+            </label>
 
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={ this.handleChange }
-              value={ email }
-            />
-          </label>
+            <label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={ this.handleChange }
+                value={ email }
+              />
+            </label>
 
-          <label htmlFor="age">
-            Idade:
-            <select
-              id="age"
-              name="age"
-              onChange={ this.handleChange }
-              value={ age }
-            >
-              <option value="">Selecione</option>
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
+            { this.renderSelectInput() }
+          </fieldset>
 
-          <label htmlFor="anecdote">
-            Anedota:
-            <textarea
-              id="anecdote"
-              name="anecdote"
-              onChange={ this.handleChange }
-              value={ anecdote }
-            />
-          </label>
+          <fieldset>
+            <legend>Texto e arquivos</legend>
+            <label htmlFor="anecdote">
+              Anedota:
+              <textarea
+                id="anecdote"
+                name="anecdote"
+                onChange={ this.handleChange }
+                value={ anecdote }
+              />
+            </label>
+
+            <input type="file" />
+          </fieldset>
 
           <label htmlFor="terms">
             <input
@@ -87,8 +103,6 @@ class Form extends Component {
             />
             Concordo com termos e acordos
           </label>
-
-          <input type="file" />
         </form>
       </div>
     );
