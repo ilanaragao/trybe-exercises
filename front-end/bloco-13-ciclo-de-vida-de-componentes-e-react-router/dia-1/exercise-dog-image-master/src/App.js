fd3422896580f49ae14101d7cf3b830b239a8f85
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-11 13:37:23 -0300

    Exercício 1 do dia 13.1

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-1/exercise-dog-image-master/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-1/exercise-dog-image-master/src/App.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    };
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((data) => data.json())
+      .then((result) => this.setState({ data: result }));
+  }
+
+  render() {
+    const { data } = this.state;
+    const { message } = data;
+    if (data === '') return 'loading...';
+    return (
+      <div>
+        <p>Doguinhos</p>
+        <button type="button" onClick={ this.fetchDog }>
+          Novo doguinho!
+        </button>
+        <div>
+          <img src={ message } alt="Random dog" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
