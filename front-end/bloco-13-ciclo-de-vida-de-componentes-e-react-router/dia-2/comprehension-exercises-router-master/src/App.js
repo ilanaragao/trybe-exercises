a0f33502bb60dbcb67b86ccff518c041ca1002d2
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-13 20:16:06 -0300

    Exercício 6 do dia 13.2

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2/comprehension-exercises-router-master/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2/comprehension-exercises-router-master/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { About, Home, Users } from './components';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/users'>Users</Link></li>
+        </ul>
+      </nav>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
         <Route path="/users" component={ Users } />
