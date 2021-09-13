d1331b784e8a56fb85126afcf1707f537dd8c1c5
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-13 20:51:34 -0300

    Finaliza dia 13.2

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2/comprehension-exercises-router-master/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2/comprehension-exercises-router-master/src/App.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import { About, Home, Users } from './components';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/users'>Users</Link></li>
-        </ul>
-      </nav>
-      <switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={ (props) => <Users { ...props } greetingMessage="Good Morning" /> } />
-      </switch>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/strict-access">Strict Access</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route
+            path="/users/:id"
+            render={(props) => (
+              <Users {...props} greetingMessage="Good Morning" />
+            )}
+          />
+          <Route
+            path="/strict-access"
+            render={() => (
+              <StrictAccess user={{ username: 'joao', password: '1234' }} />
+            )}
+          />
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     );
   }
