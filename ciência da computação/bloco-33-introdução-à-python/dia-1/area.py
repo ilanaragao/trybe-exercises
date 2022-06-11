b781dd5d9f9f2873d54d24084382bdbe8c6a5697
Ilan Aragão <ilan_aragao@hotmail.com>
2022-06-10 23:47:35 -0300

    feat: add new print

--- ciência da computação/bloco-33-introdução-à-python/dia-1/area.py
+++ ciência da computação/bloco-33-introdução-à-python/dia-1/area.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":
     print("Área do quadrado:", square(10))
     print("Área do retângulo:", rectangle(2, 2))
     print("Área do círculo:", circle(3))
+    print("Área do círculo:", circle(4))
